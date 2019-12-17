b22f15e023ec83dbbaff4b81e206da0e230c7649
juliomoralez <juliomoralez@yandex.ru>
2019-12-17 15:05:16 +0400

    v1.15

--- src/main/java/pack/Bot.java
+++ src/main/java/pack/Bot.java
@@ -31,9 +31,9 @@ public class Bot extends TelegramLongPollingBot {
     private static AgentDao agentDao;
 
     Bot(){
-        System.getProperties().put( "proxySet", "true" );
-        System.getProperties().put( "socksProxyHost", "127.0.0.1" );
-        System.getProperties().put( "socksProxyPort", "9150" );
+//        System.getProperties().put( "proxySet", "true" );
+//        System.getProperties().put( "socksProxyHost", "127.0.0.1" );
+//        System.getProperties().put( "socksProxyPort", "9150" );
 
         agentDao = new AgentDaoMemory();
       //  System.loadLibrary("opencv_java411");
